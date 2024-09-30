a8e9d8524dc53801e2541ee9532bdf079c05646f
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-09-30 19:46:53 -0300

    Criacao inicial carteirinha

---
+++ qr_code.py
@@ -0,0 +1,4 @@
+import segno
+
+qrcode = segno.make_qr("RU295656771<<<LEONARDO FERREIRA DA SILVA<<VAL30/06/2028<<<CURSO SUPERIOR DE TECNOLOGIA EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS")
+qrcode.save('welcome.png', scale=10)
